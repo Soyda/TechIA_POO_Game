9d18968cdcc304fbd7cff6f133ea80c363f7b37c
Steven <stevendruesne@gmail.com>
2021-12-08 14:50:52 +0100

    Created class and test files for game, inventory and score

---
+++ test/test_class_game.py
@@ -0,0 +1,9 @@
+import pytest
+
+# @pytest.fixture
+# def vehicule_test():
+#    return exo.Vehicule("yaris", "toyota", "grise", 20000, 0.15)
+
+# class TestGame():
+#     def test_init(self, vehicule_test):
+#         pass
---
+++ test/test_class_score.py
@@ -0,0 +1,9 @@
+import pytest
+
+# @pytest.fixture
+# def score_test():
+#    return exo.Vehicule("yaris", "toyota", "grise", 20000, 0.15)
+
+# class TestVehicule():
+#     def test_init(self, score_test):
+#         pass
