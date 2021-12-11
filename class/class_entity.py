0916febf00cca52d17daa01f7588c68efbc93e23
Ayoub <ayoubhaddou1@gmail.com>
2021-12-11 21:49:36 +0100

    Class + test fait

---
+++ class/class_entity.py
@@ -0,0 +1,110 @@
+from abc import abstractmethod, ABCMeta
+from typing import ClassVar 
+from dataclasses import dataclass , field
+from random import randint , choice  
+
+ 
+@dataclass 
+class Character(metaclass=ABCMeta):
+    name : str 
+    HP : int 
+    lvl : int 
+    is_dead : bool = False 
+    
+    @property
+    def name(self) -> str:
+        return self._name
+    @name.setter
+    def name(self, value):
+        if not isinstance(value,str):
+            raise ValueError('name should be a string')
+        else:
+            self._name = value 
+
+    @property
+    def HP(self) -> int:
+        return self._HP
+    @HP.setter
+    def HP(self,value):
+        if not isinstance(value,int):
+            raise ValueError('HP should be an int')
+        else:
+            self._HP = value
+
+    @property
+    def lvl(self) -> int:
+        return self._lvl
+    @lvl.setter
+    def lvl(self,value):
+        if not isinstance(value,int):
+            raise ValueError('lvl should be an int')
+        else:
+            self._lvl = value 
+        
+    def check_hp(self) :
+        for i in self.list_enemies : 
+            if i['HP'] <= 0 :
+                i['HP'] = 0 
+                i['is_dead'] = True 
+            print( f"{i['Name']} is dead")
+
+            
+
+
+@dataclass
+class Enemies(Character):
+    list_enemies : list = field(default_factory=list)
+
+    @staticmethod
+    def enemy_attack(x):
+        return x - randint(4,9)  
+    def gen(self):
+        c=0 
+        name_list= ["Pythosore","Devosore","Simplosore","Tiranosor","Bigbob","Jevaismourir"]
+        self.list_enemies.append({"Name" : self.name , "HP" : self.HP, 'Level' : self.lvl , 'is_dead' : self.is_dead})
+        while c < randint(2,3):
+            self.list_enemies.append({ "Name" : choice(name_list) , "HP" : randint(35,50), 'Level' : self.lvl , 'is_dead' : self.is_dead})
+            c += 1 
+        return self
+    def unique(self):
+        self.list_enemies.append({"Name" : self.name , "HP" : self.HP, 'Level' : self.lvl , 'is_dead' : self.is_dead})
+        return self
+
+
+
+
+@dataclass
+class Player(Character):
+
+    @staticmethod
+    def attack_big_punch(x) : 
+        ''' Function to attack enemy by random between 5 and 10   
+        ---------------  
+        x should be an int  (Like x = enemy_HP) '''
+        x -= randint(5,10)
+        return x  
+
+    @staticmethod
+    def attack_lightning(x):
+        ''' Function to attack group of enemy by random damages between 8 and 16
+        ---------------  
+        x should be an int  (Like x = enemy_HP)   '''
+        return x - randint(8,16)
+
+    @staticmethod
+    def fire_ball(x):
+        return x - randint(8,16)
+
+    @staticmethod
+    def use_potion(self):
+        '''  This function add 15HP to HP_player 
+        ---------------  
+        x should be instance of class (Like x = player_hp)  '''
+        return self.HP + 15
+
+    @staticmethod
+    def use_maxi_potion(self):
+        '''  This function add 50HP to HP_player 
+        ---------------  
+        x should be an int  (Like x = player_hp)  '''
+        return self.HP + 50
---
+++ class/test_class_ayoub.py
@@ -0,0 +1,95 @@
+import pytest
+import os
+
+from class_entity import Enemies, Character, Player 
+
+
+@pytest.fixture
+def TestCharacter():
+    return Character('Mytest',50,0)
+
+@pytest.fixture
+def enemies_test():
+    return Enemies('First monster',25,0)
+
+
+@pytest.fixture
+def player_test():
+    return Player('TestPlayer',50,0)
+
+
+
+class TestCharactere:
+    def test_init(self, TestCharacter):
+        assert TestCharacter.HP == 50
+        assert TestCharacter.name == "Mytest"
+        assert TestCharacter.is_dead == False
+        assert TestCharacter.lvl == 0
+        with pytest.raises(ValueError):
+            Enemies(10,55,'a')
+        with pytest.raises(ValueError):
+            Enemies("PlayerTest")
+        with pytest.raises(ValueError):
+            Enemies("Player",0)
+
+
+class TestEnemies:
+    def test_init(self, enemies_test):
+        assert enemies_test.HP == 25 
+        assert enemies_test.name == "First monster"
+        assert enemies_test.is_dead == False
+        with pytest.raises(ValueError):
+            Enemies(10,55)
+        with pytest.raises(ValueError):
+            Enemies("PlayerTest",'15')
+
+    def test_check_hp(self):
+        enemies_test_dead = Enemies('First monster',0,0)
+        assert enemies_test_dead.unique().check_hp() == f"{enemies_test_dead.list_enemies[0]['Name']} is dead"
+
+    def test_enemy_attack(x, enemies_test):
+        x = 20 
+        assert enemies_test.enemy_attack(x) <= 16 and enemies_test.enemy_attack(x) >= 11
+        x = "20"
+        with pytest.raises(TypeError):
+            enemies_test.enemy_attack(x)
+
+    def test_gen(self,enemies_test):
+        enemies_group = enemies_test.gen().list_enemies
+        assert len(enemies_group) > len(Enemies('MonsterTest',25,0).list_enemies)
+
+    def test_unique(self,enemies_test):
+        enemies_group = enemies_test.unique().list_enemies
+        assert len(enemies_group) > len(Enemies('MonsterTest',25,0).list_enemies)
+        assert enemies_group[0]['HP'] == 25 
+
+
+class TestPlayer:
+    def test_init(self, player_test):
+        assert player_test.HP == 50 
+        assert player_test.name == "TestPlayer"
+        assert player_test.is_dead == False
+        with pytest.raises(ValueError):
+            Player(99,99)
+        with pytest.raises(ValueError):
+            Player("PlayerTest")
+        with pytest.raises(ValueError):
+            Player("PlayerTest","99",0)
+        with pytest.raises(AttributeError):
+            Player("PlayerTest",99,0).use_potion(51)
+        
+
+    def test_attack_big_punch(self,enemies_test,player_test):
+        assert player_test.attack_big_punch(enemies_test.unique().list_enemies[0]['HP']) < enemies_test.unique().list_enemies[0]['HP']
+        with pytest.raises(TypeError):
+            player_test.attack_big_punch("51")        
+    def test_attack_lightning(self,enemies_test,player_test):  
+        assert player_test.attack_lightning(enemies_test.unique().list_enemies[0]['HP']) < enemies_test.unique().list_enemies[0]['HP']
+    def test_fire_ball(self,enemies_test,player_test):
+        assert player_test.fire_ball(enemies_test.unique().list_enemies[0]['HP']) < enemies_test.unique().list_enemies[0]['HP']
+    def test_use_potion(self,player_test):
+        assert player_test.use_potion(player_test) > player_test.HP
+    def test_use_maxi_potion(self, player_test):
+        assert player_test.use_maxi_potion(player_test) > player_test.HP
+        with pytest.raises(AttributeError):
+            player_test.use_maxi_potion("51") 
